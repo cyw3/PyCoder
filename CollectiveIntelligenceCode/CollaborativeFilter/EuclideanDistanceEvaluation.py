8b7e0a2ebd112e9f511031551797e40bf6551c64
cyw3 <2927096163@qq.com>
2016-01-19 20:46:45 +0800

    Python Application.

---
+++ CollectiveIntelligenceCode/CollaborativeFilter/EuclideanDistanceEvaluation.py
@@ -0,0 +1,30 @@
+﻿
+#欧几里得距离评价
+#以经过人们一致评价的物品为坐标轴，将参与的【评价的人绘制图中，考察彼此间的距离远近
+#偏好空间，求欧几里得距离
+
+#0-1
+from math import sqrt
+import recommendations
+
+def sim_distance(prefs, person1, person2):
+    #得到shared_items列表
+    si={}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
+
+    #如果两者没有共同之处，则返回0
+    if len(si)==0:
+        return 0
+
+    #计算所有差值的平方和,for特殊的用法
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
+                        for item in prefs[person1] if item in prefs[person2]])
+
+    return 1/(1+sqrt(sum_of_squares))
+
+
+#测试
+#reload(recommendations)
+print sim_distance(recommendations.critics,'Lisa Rose','Gene Seymour')
---
+++ CollectiveIntelligenceCode/CollaborativeFilter/PearsonCorrelationEvaluation.py
@@ -0,0 +1,54 @@
+﻿#皮尔逊相关度评价，使用皮尔逊相关系数
+#判断两组数据与某一直线拟合程度的一种度量。当数据不是太规范是，会倾向于更好的结果
+#该直线，是最佳拟合线。原则是尽可能的靠近图上的所有坐标点
+
+#修正了 夸大分值
+
+#以用户为坐标轴
+
+#具体步骤：
+#1、找到两者都评价过的 物品
+#2、计算两者的评分总和、平方和，评分乘积之和
+#3、计算皮尔逊系数
+
+from math import sqrt
+
+#返回一个-1~1的值
+def sim_pearson(prefs,p1,p2):
+    si={}
+    #两者共同之处
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item]=1
+
+    n=len(si)
+
+    if n==0:return 1
+
+    #评分总和
+    sum1=sum([prefs[p1][it]for it in si])
+    sum2=sum([prefs[p2][it]for it in si])
+
+    #求平方和
+    sum1Sq=sum([pow(prefs[p1][it],2)for it in si])
+    sum2Sq=sum([pow(prefs[p2][it],2)for it in si])
+
+    #求对应乘积之和
+    pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    #计算皮尔逊相关系数
+    num=pSum-(sum1*sum2/n)
+    den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    if den==0:return 0
+
+    r=num/den
+
+    return r
+
+#test
+import recommendations
+#reload(recommendations)
+print sim_pearson(recommendations.critics,'Lisa Rose','Gene Seymour')
+
+    
+    
